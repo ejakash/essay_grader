6b6f1e9c29e3de535ebf49144039957109642796
aedach2 <aedach2@uic.edu>
2018-03-31 01:53:23 -0500

    Added the spell checker module.

--- src/main/java/AutograderMain.java
+++ src/main/java/AutograderMain.java
@@ -2,6 +2,10 @@ import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.POS;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -11,13 +15,28 @@ import edu.stanford.nlp.util.CoreMap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class AutograderMain {
+
+    private static IDictionary dictionary = null;
+    private static HashSet<String> closedWords_en = null;
+    static {
+        try{closedWords_en = new HashSet<>(Files.readAllLines(Paths.get("lib/closed_class.txt")));}
+        catch (IOException e) {e.printStackTrace();}
+        URL url = null;
+        try{ url = new URL("file", null, "lib/dict"); }
+        catch(MalformedURLException e){ e.printStackTrace(); }
+        if(url != null)dictionary = new Dictionary(url);
+    }
 
     public static List<String> tokenize(String text) {
         Properties props = new Properties();
@@ -120,6 +139,40 @@ public class AutograderMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static double spellCheck(String essay) {
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(essay);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+        List<String> tokenLemma = document.get(CoreAnnotations.TokensAnnotation.class).stream().map(token -> token.get(CoreAnnotations.LemmaAnnotation.class)).collect(Collectors.toList());
+        Long correctCount = tokenLemma.stream().filter(AutograderMain::isCorrect).count();
+        return (double)correctCount/tokenLemma.size() * 4;
+    }
+
+    private static boolean isCorrect(String text)  {
+        if(closedWords_en == null) System.out.println("Unable to read the closed word list");
+        else if(closedWords_en.contains(text.toLowerCase())) return true;
+        if(dictionary == null) {
+            System.out.println("Unable to read the wordnet dictionary");
+            //Assume all words to the true if the spelling checker fails to load
+            return true;
+        }
+        try {dictionary.open();}
+        catch (IOException e) {e.printStackTrace();}
+        POS[] posList = {POS.NOUN, POS.VERB, POS.ADJECTIVE, POS.ADVERB};
+        for(POS pos : posList) {
+            IIndexWord idxWord = dictionary.getIndexWord(text, pos);
+            if(idxWord != null) return true;
+        }
+        dictionary.close();
+        return false;
     }
 
 }
